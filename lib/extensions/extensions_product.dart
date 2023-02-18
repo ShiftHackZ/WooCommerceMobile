db755a436efe4312eb6646334d62296bb847d8af
ShiftHackZ <dmitriy@moroz.cc>
2023-02-18 20:29:47 +0200

    Order card status designed

--- lib/extensions/extensions_product.dart
+++ lib/extensions/extensions_product.dart
@@ -1,4 +1,8 @@
 import 'package:html/parser.dart';
+import 'package:intl/intl.dart' as intl;
+
+const _dateFormatSeverDefault = "yyyy-MM-dd'T'HH:mm:SS";
+const _dateFormatUiDefault = "dd MMMM yyyy',' HH:mm";
 
 String parseHtml(String htmlDescription) {
   final document = parse(htmlDescription);
@@ -13,4 +17,23 @@ String parseTotals(
   var partBase = totals.substring(0, totals.length - fraction);
   var partFraction = totals.substring(totals.length - fraction, totals.length);
   return '$partBase$delimiter$partFraction';
+}
+
+String convertDate(
+  String? input, {
+  String inFormat = _dateFormatSeverDefault,
+  String outFormat = _dateFormatUiDefault,
+  bool inFormatUTC = false,
+  bool convertToLocal = false,
+  String? locale,
+}) {
+  if (input == null || input.isEmpty) return '';
+  try {
+    DateTime dateUTC = intl.DateFormat(inFormat).parse(input, inFormatUTC);
+    return intl
+        .DateFormat(outFormat, locale)
+        .format(convertToLocal ? dateUTC.toLocal() : dateUTC);
+  } catch (e) {
+    return '';
+  }
 }
