44e04b3668843fcbf4f7f3da75698ef10499f112
ShiftHackZ <dmitriy@moroz.cc>
2023-02-19 19:15:22 +0200

    Review add / viewing flow polishing

--- lib/screens/reviews/review_add.dart
+++ lib/screens/reviews/review_add.dart
@@ -6,6 +6,7 @@ import 'package:wooapp/datasource/product_review_data_source.dart';
 import 'package:wooapp/extensions/extensions_context.dart';
 import 'package:wooapp/locator.dart';
 import 'package:wooapp/widget/widget_dialog.dart';
+import 'package:wooapp/widget/widget_loader_full_screen.dart';
 
 class AddReviewScreen extends StatefulWidget {
   final int productId;
@@ -20,164 +21,186 @@ class _AddReviewScreenState extends State<AddReviewScreen> {
   final ProductReviewDataSource _ds = locator<ProductReviewDataSource>();
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
   final TextEditingController _reviewController = TextEditingController();
 
+  bool _loading = false;
   double _rating = 5;
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      leading: BackButton(
-        color: WooAppTheme.colorToolbarForeground,
-      ),
-      title: Text(
-        'review_add',
-        style: TextStyle(
-          color: WooAppTheme.colorToolbarForeground,
+  Widget build(BuildContext context) => Stack(
+        children: [
+          _buildScreenContent(),
+          if (_loading) WooFullScreenLoader(),
+        ],
+      );
+
+  Widget _buildScreenContent() => Scaffold(
+        appBar: AppBar(
+          leading: BackButton(
+            color: WooAppTheme.colorToolbarForeground,
+          ),
+          title: Text(
+            'review_add',
+            style: TextStyle(
+              color: WooAppTheme.colorToolbarForeground,
+            ),
+          ).tr(),
+          backgroundColor: WooAppTheme.colorToolbarBackground,
         ),
-      ).tr(),
-      backgroundColor: WooAppTheme.colorToolbarBackground,
-    ),
-    backgroundColor: WooAppTheme.colorCommonBackground,
-    body: SafeArea(
-      child: Center(
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    controller: _reviewController,
-                      // cursorColor: Colors.blue,
-                    style: TextStyle(
-                      fontSize: 16,
-                        // color: Colors.white
-                    ),
-                    keyboardType: TextInputType.multiline,
-                    minLines: 3,
-                    maxLines: 8,
-                    decoration: _decorate(tr('review')),
-                    maxLength: 600,
-                    validator: (value) {
-                      if (value.toString().isEmpty) {
-                        return 'Review is required';
-                      }
-                      return null;
-                    }
-                  ),
-                  RatingBar.builder(
-                      initialRating: _rating,
-                      minRating: 1,
-                      itemCount: 5,
-                      allowHalfRating: false,
-                      itemBuilder: (context, _) => Icon(
-                        Icons.star,
-                        color: Colors.amber,
+        backgroundColor: WooAppTheme.colorCommonBackground,
+        body: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        controller: _reviewController,
+                        // cursorColor: Colors.blue,
+                        style: TextStyle(
+                          fontSize: 16,
+                          // color: Colors.white
+                        ),
+                        keyboardType: TextInputType.multiline,
+                        minLines: 3,
+                        maxLines: 8,
+                        decoration: _decorate(tr('review')),
+                        maxLength: 600,
+                        validator: (value) {
+                          if (value.toString().isEmpty) {
+                            return 'Review is required';
+                          }
+                          return null;
+                        },
                       ),
-                      onRatingUpdate: (rating) {
-                        print('newRating = $rating');
-                        setState(() {
-                          _rating = rating;
-                        });
-                      }
+                      RatingBar.builder(
+                        initialRating: _rating,
+                        minRating: 1,
+                        itemCount: 5,
+                        allowHalfRating: false,
+                        itemBuilder: (context, _) => Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        onRatingUpdate: (rating) {
+                          print('newRating = $rating');
+                          setState(() {
+                            _rating = rating;
+                          });
+                        },
+                      ),
+                      _bottomBar()
+                    ],
                   ),
-                  _bottomBar()
-                ],
+                ),
               ),
             ),
           ),
         ),
-      ),
-    )
-  );
+      );
 
   Widget _bottomBar() => Container(
-    padding: EdgeInsets.only(bottom: 8, top: 32, right: 32, left: 32),
-    height: 92,
-    child: ElevatedButton(
-      onPressed: () {
-        if (_formKey.currentState!.validate()) {
-          hideKeyboardForce(context);
-          _postReview();
-        }
-      },
-      child: Container(
-          alignment: Alignment.center,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // FaIcon(FontAwesomeIcons.commentMedical),
-              // SizedBox(width: 8),
-              Text(
-                'review_add_action',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: WooAppTheme.colorPrimaryForeground,
+        padding: EdgeInsets.only(bottom: 8, top: 32, right: 32, left: 32),
+        height: 92,
+        child: ElevatedButton(
+          onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              hideKeyboardForce(context);
+              _postReview();
+            }
+          },
+          child: Container(
+              alignment: Alignment.center,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // FaIcon(FontAwesomeIcons.commentMedical),
+                  // SizedBox(width: 8),
+                  Text(
+                    'review_add_action',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: WooAppTheme.colorPrimaryForeground,
+                    ),
+                  ).tr(),
+                ],
+              )),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(
+              WooAppTheme.colorPrimaryBackground,
+            ),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(36.0),
+                side: BorderSide(
+                  color: WooAppTheme.colorPrimaryBackground,
                 ),
-              ).tr(),
-            ],
-          )
-      ),
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(
-          WooAppTheme.colorPrimaryBackground,
-        ),
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(36.0),
-            side: BorderSide(
-              color: WooAppTheme.colorPrimaryBackground,
+              ),
             ),
           ),
         ),
-      ),
-    ),
-  );
+      );
 
   void _postReview() {
-    _ds.addReview(widget.productId, _rating, _reviewController.text)
-        .then((value) {
-          Navigator.of(context)..pop()..pop();
-          showDialog(
-            context: context,
-            builder: (ctx) => WooDialog(
-              title: 'Thank you!',
-              text: 'Your review posted successfully.',
-            ),
-          );
-        }).catchError((error) {
-          showDialog(
-            context: context,
-              builder: (ctx) => WooDialog(
-                title: 'Oops...',
-                text: 'There was some issue while posting your review. Please, try again.',
-              ),
-          );
-        });
+    setState(() => _loading = true);
+    _ds.addReview(
+      widget.productId,
+      _rating,
+      _reviewController.text,
+    ).then((value) {
+      setState(() => _loading = false);
+      Navigator.of(context)
+        ..pop()
+        ..pop();
+      showDialog(
+        context: context,
+        builder: (ctx) => WooDialog(
+          title: tr('thank_you'),
+          text: tr('review_submit_success'),
+        ),
+      );
+    }).catchError((error) {
+      setState(() => _loading = false);
+      showDialog(
+        context: context,
+        builder: (ctx) => WooDialog(
+          title: tr('error_state_oops'),
+          text:
+              'There was some issue while posting your review. Please, try again.',
+        ),
+      );
+    });
   }
 
-  InputDecoration _decorate(String label, {String prefix = ''}) => InputDecoration(
-      enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(width: 0.0)
-      ),
-      focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(width: 1.0)
-      ),
-      errorBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.redAccent, width: 1.0)
-      ),
-      focusedErrorBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.redAccent, width: 1.0)
-      ),
-      // labelStyle: TextStyle(color: Colors.white),
-      labelText: label,
-      prefixText: prefix
-  );
+  InputDecoration _decorate(String label, {String prefix = ''}) =>
+      InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(width: 0.0),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(width: 1.0),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.redAccent,
+            width: 1.0,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.redAccent,
+            width: 1.0,
+          ),
+        ),
+        // labelStyle: TextStyle(color: Colors.white),
+        labelText: label,
+        prefixText: prefix,
+      );
 }
--- lib/widget/widget_review_item.dart
+++ lib/widget/widget_review_item.dart
@@ -14,80 +14,112 @@ class ReviewItemWidget extends StatelessWidget {
   ReviewItemWidget(this.review);
 
   @override
-  Widget build(BuildContext context) => Column(
-    children: [
-      Padding(
-        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-        child: Row(
+  Widget build(BuildContext context) => Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 12,
+          horizontal: 12,
+        ),
+        margin: EdgeInsets.symmetric(
+          horizontal: 8,
+          vertical: 4,
+        ),
+        decoration: BoxDecoration(
+          color: WooAppTheme.colorSecondaryBackground.withOpacity(0.5),
+          borderRadius: BorderRadius.all(
+            Radius.circular(8.0),
+          ),
+        ),
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            _buildAvatar(),
-            SizedBox(width: 12),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildAvatar(),
+                SizedBox(width: 12),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(top: 2),
+                            child: Text(
+                              '${review.reviewer}',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                                color: WooAppTheme.colorCardProductText,
+                              ),
+                            ),
+                          ),
+                          Spacer(),
+                          RatingBarIndicator(
+                            rating: review.rating,
+                            itemBuilder: (context, index) => Icon(
+                              Icons.star,
+                              color: WooAppTheme.colorRatingActive,
+                            ),
+                            unratedColor: WooAppTheme.colorRatingNonActive,
+                            itemCount: 5,
+                            itemSize: 16,
+                            direction: Axis.horizontal,
+                          ),
+                          SizedBox(width: 6),
+                          Text(
+                            '${review.rating}',
+                            style: TextStyle(
+                              color: WooAppTheme.colorRatingText,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 6),
                       Text(
-                        '${review.reviewer}',
-                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-                      ),
-                      Spacer(),
-                      RatingBarIndicator(
-                        rating: review.rating,
-                        itemBuilder: (context, index) => Icon(
-                          Icons.star,
-                          color: WooAppTheme.colorRatingActive,
+                        parseHtml(review.review),
+                        style: TextStyle(
+                          color: WooAppTheme.colorCardProductText,
                         ),
-                        unratedColor: WooAppTheme.colorRatingNonActive,
-                        itemCount: 5,
-                        itemSize: 12,
-                        direction: Axis.horizontal,
                       ),
-                      Text(' ${review.rating.toString()}')
                     ],
                   ),
-                  SizedBox(height: 8),
-                  Text(parseHtml(review.review)),
-                ],
-              )
+                ),
+              ],
             ),
           ],
         ),
-      ),
-      Divider()
-    ],
-  );
+      );
 
   Widget _buildAvatar() => CircleAvatar(
-    backgroundColor: Colors.blueGrey,
-    radius: avatarSize,
-    child: ClipRRect(
-      borderRadius: BorderRadius.all(Radius.circular(avatarSize)),
-      child: CachedNetworkImage(
-        imageUrl: review.reviewerAvatar.x48,
-        imageBuilder: (context, imageProvider) => Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: imageProvider,
-              fit: BoxFit.cover,
+        backgroundColor: Colors.blueGrey,
+        radius: avatarSize,
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(avatarSize)),
+          child: CachedNetworkImage(
+            imageUrl: review.reviewerAvatar.x48,
+            imageBuilder: (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            placeholder: (context, url) => Shimmer(
+              duration: Duration(seconds: 1),
+              enabled: true,
+              direction: ShimmerDirection.fromLTRB(),
+              color: WooAppTheme.colorShimmerForeground,
+              child: Container(
+                color: WooAppTheme.colorShimmerBackground,
+              ),
+            ),
+            errorWidget: (context, url, error) => Center(
+              child: Icon(Icons.error),
             ),
           ),
         ),
-        placeholder: (context, url) => Shimmer(
-          duration: Duration(seconds: 1),
-          enabled: true,
-          direction: ShimmerDirection.fromLTRB(),
-          color: WooAppTheme.colorShimmerForeground,
-          child: Container(
-            color: WooAppTheme.colorShimmerBackground,
-          ),
-        ),
-        errorWidget: (context, url, error) => Center(
-          child: Icon(Icons.error),
-        ),
-      ),
-    ),
-  );
+      );
 }
