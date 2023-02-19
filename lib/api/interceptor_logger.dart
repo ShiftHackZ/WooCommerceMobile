b3ccb3479314c5a114b4140b192a51ae24996b5e
ShiftHackZ <dmitriy@moroz.cc>
2023-02-19 20:54:43 +0200

    Interceptor print

--- lib/api/interceptor_logger.dart
+++ lib/api/interceptor_logger.dart
@@ -1,38 +1,41 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
 class PrinterInterceptor extends Interceptor {
 
   @override
   Future onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
-    print('''------------------- START REQUEST
+    if (kDebugMode) print('''-------------------> HTTP REQUEST
 Method: ${options.method}
 Headers: ${options.headers}
 Path: ${options.baseUrl}${options.path}
 Query parameters: ${options.queryParameters}
 Data: ${options.data}
 Content type: ${options.contentType}
-------------------- END HTTP REQUEST''');
+-------------------> END HTTP REQUEST''');
     return super.onRequest(options, handler);
   }
 
   @override
   Future onResponse(Response response, ResponseInterceptorHandler handler) async {
-    print('''------------------- START RESPONSE
+    if (kDebugMode) print('''<------------------- HTTP RESPONSE
 Status code: ${response.statusCode}
+Headers: ${response.headers}
 Response data: ${response.data}
 Path: ${response.realUri}
-------------------- END RESPONSE''');
-    super.onResponse(response, handler);//Headers: ${response.headers}
+<------------------- END HTTP RESPONSE''');
+    super.onResponse(response, handler);
   }
 
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) async {
-    print('''------------------- START ERROR
+    if (kDebugMode) print('''<------------------- HTTP ERROR
+Headers: ${err.response?.headers.map ?? {}}
 Response: ${err.response}
 Error type: ${err.type}
 Error message: ${err.message}
 Path: ${err.response?.realUri}
-------------------- END ERROR''');
+<------------------- END HTTP ERROR''');
     super.onError(err, handler);
   }
 }
