44a5acb179203619bf8a59f5da0f71fe53ca7ac3
ShiftHackZ <dmitriy@moroz.cc>
2023-02-23 22:19:36 +0200

    Category feed style prototype

--- lib/widget/widget_product_feed.dart
+++ lib/widget/widget_product_feed.dart
@@ -5,6 +5,7 @@ import 'package:page_view_indicators/circle_page_indicator.dart';
 import 'package:shimmer_animation/shimmer_animation.dart';
 import 'package:wooapp/config/theme.dart';
 import 'package:wooapp/model/product.dart';
+import 'package:wooapp/widget/widget_price.dart';
 import 'package:wooapp/widget/widget_price_product.dart';
 import 'package:wooapp/widget/widget_product_actions.dart';
 
@@ -12,16 +13,19 @@ class ProductFeedWidget extends StatelessWidget {
   final _sliderController = PageController();
   final _sliderNotifier = ValueNotifier<int>(0);
 
-  final Product product;
   final Function() onProductAction;
   final Function(int index) onImageClicked;
   final bool displayWish;
 
+  final Product? product;
+  final CategoryProduct? categoryProduct;
+
   ProductFeedWidget({
-    required this.product,
     required this.onProductAction,
     required this.onImageClicked,
     this.displayWish = false,
+    this.product,
+    this.categoryProduct,
   });
 
   @override
@@ -30,32 +34,37 @@ class ProductFeedWidget extends StatelessWidget {
         children: [
           Container(
             height: 280,
-            child: _imageSlider(context, product),
+            child: _imageSlider(context),
           ),
           SizedBox(height: 16),
           Row(
             children: [
               SizedBox(width: 16),
-              ProductPriceWidget.withProduct(product),
+              if (product != null)
+                ProductPriceWidget.withProduct(product!),
+              if (categoryProduct != null)
+                PriceWidget(categoryProduct!.price, categoryProduct!.salePrice),
               Spacer(),
-              RatingBarIndicator(
-                rating: product.rating,
-                itemBuilder: (context, index) => Icon(
-                  Icons.star,
-                  color: WooAppTheme.colorRatingActive,
+              if (product != null)
+                RatingBarIndicator(
+                  rating: product?.rating ?? 0.0,
+                  itemBuilder: (context, index) => Icon(
+                    Icons.star,
+                    color: WooAppTheme.colorRatingActive,
+                  ),
+                  unratedColor: WooAppTheme.colorRatingNonActive,
+                  itemCount: 5,
+                  itemSize: 16,
+                  direction: Axis.horizontal,
                 ),
-                unratedColor: WooAppTheme.colorRatingNonActive,
-                itemCount: 5,
-                itemSize: 16,
-                direction: Axis.horizontal,
-              ),
-              Text(
-                ' ${product.rating.toString()}',
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  color: WooAppTheme.colorRatingText,
+              if (product != null)
+                Text(
+                  ' ${product?.rating}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    color: WooAppTheme.colorRatingText,
+                  ),
                 ),
-              ),
               SizedBox(width: 16),
             ],
           ),
@@ -65,7 +74,7 @@ class ProductFeedWidget extends StatelessWidget {
               horizontal: 16,
             ),
             child: Text(
-              product.name,
+              product?.name ?? categoryProduct?.name ?? '',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.w600,
@@ -73,21 +82,22 @@ class ProductFeedWidget extends StatelessWidget {
               ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 8.0,
-              horizontal: 16,
+          if (product != null)
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 8.0,
+                horizontal: 16,
+              ),
+              child: ProductActionsWidget(
+                product: product!,
+                changeCallback: () => onProductAction(),
+                displayWish: displayWish,
+              ),
             ),
-            child: ProductActionsWidget(
-              product: product,
-              changeCallback: () => onProductAction(),
-              displayWish: displayWish,
-            ),
-          ),
         ],
       );
 
-  Widget _imageSlider(BuildContext context, Product product) => GestureDetector(
+  Widget _imageSlider(BuildContext context) => GestureDetector(
         onTap: () => onImageClicked(_sliderController.page?.toInt() ?? 0),
         child: Stack(
           children: [
@@ -95,13 +105,18 @@ class ProductFeedWidget extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               controller: _sliderController,
               children: [
-                for (var image in product.images) _imageSingle(image.src)
+                if (product != null)
+                  for (var image in product!.images)
+                    _imageSingle(image.src),
+
+                if (categoryProduct != null)
+                  _imageSingle(categoryProduct?.image ?? ''),
               ],
               onPageChanged: (index) {
                 _sliderNotifier.value = index;
               },
             ),
-            if (product.images.length > 1)
+            if ((product?.images.length ?? 1) > 1)
               Positioned(
                 left: 0.0,
                 right: 0.0,
@@ -109,7 +124,7 @@ class ProductFeedWidget extends StatelessWidget {
                 child: Padding(
                   padding: EdgeInsets.all(8.0),
                   child: CirclePageIndicator(
-                    itemCount: product.images.length,
+                    itemCount: product?.images.length ?? 1,
                     currentPageNotifier: _sliderNotifier,
                   ),
                 ),
